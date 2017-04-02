45552d36145bdb51a683ca2204c50bd4567438b7
Tyler Gillies <tjgillies@gmail.com>
2017-04-02 09:48:13 -0700

    add edit function

--- src/status.cr
+++ src/status.cr
@@ -3,7 +3,16 @@ require "db"
 require "pg"
 require "kemal-session"
 require "markdown"
+Conn = DB.open ENV["DATABASE_URL"]
 
+class Index
+  @current_post : Hash(String, String | Int32) | Nil
+  @posts : Array(Hash(Symbol, String | Int32))
+  @current_user : User | Nil
+  def initialize(@current_post, @posts, @current_user)
+  end
+  ECR.def_to_s "index.html.ecr"
+end
 
 struct Post
   def initialize(@author_id, @body, @title, @id)
@@ -20,6 +29,7 @@ end
 
 struct User
   def initialize(@img, @name, @id)
+    
   end
 
   JSON.mapping(
@@ -33,38 +43,50 @@ end
 
 
 get "/" do |env|
-  # env.session.object("current_user", User.new("","Test user", 42))
-  current_user = env.session.object?("current_user")
-  DB.open ENV["DATABASE_URL"] do |db|
-    db.query "select img, name, body, title, posts.id, users.id from users,posts where posts.author_id = users.id order by posts.created_at desc" do |rs|
-      posts = [] of Hash(Symbol, String | Int32)
-      rs.each do 
-        posts << {
-          :img => rs.read(String), 
-          :name => rs.read(String), 
-          :body => rs.read(String), 
-          :title => rs.read(String), 
-          :id => rs.read(Int32),
-          :author_id => rs.read(Int32)
-        }
-      end  
-      render "index.html.ecr"
-    end
+  env.session.object("current_user", User.new("","Test user", 42))
+  Conn.query "select img, name, body, title, posts.id, users.id from users,posts where posts.author_id = users.id order by posts.created_at desc" do |rs|
+    posts = [] of Hash(Symbol, String | Int32)
+    rs.each do 
+      posts << {
+        :img => rs.read(String), 
+        :name => rs.read(String), 
+        :body => rs.read(String), 
+        :title => rs.read(String), 
+        :id => rs.read(Int32),
+        :author_id => rs.read(Int32)
+      }
+    end  
+    Index.new(nil, posts, env.session.object?("current_user"))
   end
+end
+
+get "/edit/:id" do |env|
+  post_title, post_body, post_id = Conn.query_one "select title, body, id from posts where posts.id = $1", env.params.url["id"], as: {String, String, Int32}
+  posts = [] of Hash(Symbol, String | Int32)
+  Conn.query "select img, name, body, title, posts.id, users.id from users,posts where posts.author_id = users.id order by posts.created_at desc" do |rs|
+    rs.each do 
+      posts << {
+        :img => rs.read(String), 
+        :name => rs.read(String), 
+        :body => rs.read(String), 
+        :title => rs.read(String), 
+        :id => rs.read(Int32),
+        :author_id => rs.read(Int32)
+      }
+    end  
+  end
+  Index.new({"title" => post_title, "body" => post_body, "id" => post_id}, posts, env.session.object?("current_user"))
 end
 
 get "/delete/:id" do |env|
   post_id = env.params.url["id"]
   
   current_user = env.session.object("current_user")
-  DB.open ENV["DATABASE_URL"] do |db|
-    db.query "select posts.id from posts,users where posts.id = $1 and posts.author_id = $2", post_id, current_user.id do |rs|
-      puts "looking for post"
-      rs.each do
-        if post_id = rs.read(Int32|Nil)
-          puts "got post id"
-          db.exec "delete from posts where id = $1", post_id
-        end
+  Conn.query "select posts.id from posts,users where posts.id = $1 and posts.author_id = $2", post_id, current_user.id do |rs|
+    puts "looking for post"
+    rs.each do
+      if post_id = rs.read(Int32|Nil)
+        Conn.exec "delete from posts where id = $1", post_id
       end
     end
   end
@@ -74,8 +96,17 @@ end
 post "/submit" do |env|
   current_user = env.session.object("current_user")
   markdown = Markdown.to_html(env.params.body["body"].as(String))
-  DB.open ENV["DATABASE_URL"] do |db|
-    db.exec "insert into posts values ($1,$2,$3)", current_user.id, markdown, env.params.body["title"]
+  Conn.exec "insert into posts values ($1,$2,$3)", current_user.id, markdown, env.params.body["title"]
+  env.redirect "/"
+end
+
+post "/edit/:id" do |env|
+  current_user = env.session.object("current_user")
+  post_id = env.params.url["id"]
+  post_body = env.params.body["body"]
+  post_title = env.params.body["title"]
+  if post_id = Conn.query_one "select id from posts where id = $1 and posts.author_id = $2", post_id, current_user.id, as: Int32
+    Conn.exec "update posts set body = $1, title = $2 where id = $3", post_body, post_title, post_id 
   end
   env.redirect "/"
 end
@@ -104,16 +135,14 @@ get "/login" do |env|
   img = user_data["avatar_url"].as_s
 
   env.session.object("current_user", User.new(img, name, id))
-  DB.open ENV["DATABASE_URL"] do |db|
-    users = [] of Int32
-    db.query "select id from users where id = $1", id do |rs|
-      rs.each do 
-        users << rs.read(Int32)
-      end  
-    end
-    if users.empty?
-      db.exec "insert into users values ($1, $2, $3)", img, name, id
-    end
+  users = [] of Int32
+  Conn.query "select id from users where id = $1", id do |rs|
+    rs.each do 
+      users << rs.read(Int32)
+    end  
+  end
+  if users.empty?
+    Conn.exec "insert into users values ($1, $2, $3)", img, name, id
   end
 
   env.redirect "/"
