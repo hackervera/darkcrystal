f7862f9405aec40a8c9f499116cfbfc31079ae17
Tyler Gillies <tjgillies@gmail.com>
2017-04-02 09:50:32 -0700

    nope

--- src/status.cr
+++ src/status.cr
@@ -43,7 +43,7 @@ end
 
 
 get "/" do |env|
-  env.session.object("current_user", User.new("","Test user", 42))
+  # env.session.object("current_user", User.new("","Test user", 42)) 
   Conn.query "select img, name, body, title, posts.id, users.id from users,posts where posts.author_id = users.id order by posts.created_at desc" do |rs|
     posts = [] of Hash(Symbol, String | Int32)
     rs.each do 
